0eb94e60446cde6f29e1eed0e97dce7d8e52fde8
hereton <totsapon.m@ku.th>
2025-05-29 16:59:43 +0700

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization for static export
+  },
+  basePath: isProd ? '/email-campaign' : '', // Replace with your GitHub repository name
+  assetPrefix: isProd ? '/email-campaign/' : '', // Replace with your GitHub repository name
 };
 
 export default nextConfig;
