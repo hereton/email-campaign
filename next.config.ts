20200bc41e3cd144d3d0c32fb1c2aaa10b2e894a
hereton <totsapon.m@ku.th>
2025-05-29 17:33:28 +0700

    sdad

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   reactStrictMode: false,
+  images: {
+    unoptimized: true, // Disable Next.js Image Optimization for static export
+  },
   basePath: isProd ? '/email-campaign' : '', // Replace with your GitHub repository name
   assetPrefix: isProd ? '' : '', // Replace with your GitHub repository name
 };
