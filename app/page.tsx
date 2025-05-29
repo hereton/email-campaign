e4567dc378acfd5d1b98122650f9743f548b8461
hereton <totsapon.m@ku.th>
2025-05-29 20:03:24 +0700

    fix bug file name

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { EmailCampaignRichText } from "@/components/emailCampaignRichText/EmailCampaignRichTextt";
+import { EmailCampaignRichText } from "@/components/emailCampaignRichText/emailCampaignRichText";
 import { useGetBasePath } from "../hooks/useGetBasePath";
 
 export default function Home() {
